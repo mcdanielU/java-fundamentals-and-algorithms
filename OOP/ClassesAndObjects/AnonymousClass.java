c3b191952d7d0e17df1ac7e4ef27a4fc6e756dde
mcdanielU <ucheelekwa2@gmail.com>
2024-12-04 20:17:37 -0500

    Anonymous classes

---
+++ OOP/ClassesAndObjects/AnonymousClass.java
@@ -0,0 +1,28 @@
+package OOP.ClassesAndObjects;
+import java.util.Arrays;
+class LightClick
+{
+    void tap(){};
+    void smartSpeech(){};
+}
+
+
+public class AnonymousClass {
+    public static void main(String[] args) {
+         int a = 10;
+     new LightClick()
+     {
+         @Override
+         public void tap()
+         {
+             System.out.println("Light was clicked at "+a+" intensity");
+         }
+
+         @Override
+         public void smartSpeech() {
+             System.out.println("AI speec woke up the light ");
+         }
+         ;
+     }.tap();
+    }
+}
